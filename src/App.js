5404af7dc24ff1baa01c951a9843ca63f265465f
Brian Lang <867983+brilang@users.noreply.github.com>
2018-09-13 20:36:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Munzee Printer
+      </div>
+    );
+  }
+}
+
+export default App;
