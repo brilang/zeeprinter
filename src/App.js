16dfb0e137d07c90b82b532570c10c9c04fd991f
Brian Lang <867983+brilang@users.noreply.github.com>
2018-09-14 20:27:12 -0700

    Add MainMenu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainMenu from './components/MainMenu/MainMenu'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MainMenu></MainMenu>
         Zee Printer
       </div>
     );
