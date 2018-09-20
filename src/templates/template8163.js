80643f62c2286356457b21b53413dff799f32ec3
Brian Lang <867983+brilang@users.noreply.github.com>
2018-09-20 16:20:34 -0700

    Add first template

---
+++ src/templates/template8163.js
@@ -0,0 +1,18 @@
+const template8163 = {
+	pageleftmargin: 5,
+	horizontalgutterinlabel: 1.9,
+	horizontalgutterbetweenlabels: 5,
+	pagetopmargin: 12.5,
+	verticalgutterbetweenlabels: 0,
+	verticalgutterlabeltop: 3,
+	verticalgutterlabelbottom: 3,
+	verticalgutterbetweencodes: 9,
+	w: 18,
+	h: 18,
+	columnsperlabel: 5,
+	columnsperpage: 2,
+	rowsperlabel: 2,
+	rowsperpage: 5
+}
+
+export default template8163
